cb390c0ff7b22cbaed36b04524e217c48bb91476
Jorge Barnaby <jorge.barnaby@genesys.com>
2014-06-18 00:01:50 -0400

    Some minor improvements.

--- server.js
+++ server.js
@@ -11,28 +11,30 @@ app
   .use(connect.logger())
   .use(vhost('s1.local.dev', local1App))
   .use(vhost('s2.local.dev', local2App))
-  .use(vhost('*.dev', otherApp));
+  .use(vhost('*.local.dev', otherApp));
 
 local1App.use(function(req, res) {
   var cookies = new Cookies(req, res);
   cookies.set('cookie_no_domain', 1);
   cookies.set('cookie_with_domain', 2, { domain: 'local.dev' });
-  res.write('Host: s1.local.dev\n');
+  res.write('Host: ' + req.headers.host + '\n');
   res.end('\n');
 });
 
 local2App.use(function(req, res) {
   var cookies = new Cookies(req, res);
-  res.write('Host: s2.local.dev\n');
+  res.write('Host: ' + req.headers.host + '\n');
   res.write('Cookie no domain: ' + cookies.get('cookie_no_domain') + '\n');
   res.write('Cookie with domain: ' + cookies.get('cookie_with_domain') + '\n');
   res.end('\n');
 });
 
 otherApp.use(function(req, res) {
-  res.end('other');
+  res.write('Host: ' + req.headers.host + '\n');
+  res.end('\n');
 });
 
 app.listen(port, function() {
   console.log('Listening to port', port);
 });
+
