ad17e58645259ad31e8545f12597758c801a7d3a
Jorge Barnaby <jorge.barnaby@genesys.com>
2014-06-17 23:52:37 -0400

    Setting and reading cookies implemented.

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-var http = require('http'),
-    connect = require('connect'),
+var connect = require('connect'),
     vhost = require('vhost'),
     port = 8080,
     app = connect(),
     local1App = connect(),
     local2App = connect(),
-    otherApp = connect();
+    otherApp = connect(),
+    Cookies = require('cookies');
 
 app
-  .use(connect.logger('dev'))
-  .use(vhost('local1.dev', local1App))
-  .use(vhost('local2.dev', local2App))
+  .use(connect.logger())
+  .use(vhost('s1.local.dev', local1App))
+  .use(vhost('s2.local.dev', local2App))
   .use(vhost('*.dev', otherApp));
 
 local1App.use(function(req, res) {
-  res.end('local1');
+  var cookies = new Cookies(req, res);
+  cookies.set('cookie_no_domain', 1);
+  cookies.set('cookie_with_domain', 2, { domain: 'local.dev' });
+  res.write('Host: s1.local.dev\n');
+  res.end('\n');
 });
 
 local2App.use(function(req, res) {
-  res.end('local2');
+  var cookies = new Cookies(req, res);
+  res.write('Host: s2.local.dev\n');
+  res.write('Cookie no domain: ' + cookies.get('cookie_no_domain') + '\n');
+  res.write('Cookie with domain: ' + cookies.get('cookie_with_domain') + '\n');
+  res.end('\n');
 });
 
 otherApp.use(function(req, res) {
